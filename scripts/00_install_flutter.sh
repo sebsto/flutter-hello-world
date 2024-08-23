19bd23f46d69200d8569e2c24ff9d1d1f87ca188
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2024-08-23 13:37:29 +0200

    fix flutter installation script

--- scripts/00_install_flutter.sh
+++ scripts/00_install_flutter.sh
@@ -1,3 +1,5 @@
+#!/bin/sh 
+
 # download and install Flutter 
 
 export FLUTTER_DIR="$HOME/flutter"
@@ -6,13 +8,13 @@ if [ -d ${FLUTTER_DIR} ]; then # we are running inside AWS CodeBuild
 		exit -1
 else
     echo "Flutter not found."
+    pushd $HOME
 		
     echo "Downloading Flutter."
 		curl https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_arm64_3.24.1-stable.zip -O flutter_macos_arm64_3.24.1-stable.zip -s
 
     echo "Unziping Flutter."
-		unzip -qq -l flutter_macos_arm64_3.24.1-stable.zip -d "$FLUTTER_DIR" 
+		unzip -qq flutter_macos_arm64_3.24.1-stable.zip -d "$HOME" 
+    
+    popd
 fi
-
-
-
