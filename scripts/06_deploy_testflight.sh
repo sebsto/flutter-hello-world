d7e6e5f335f6955d0dbceeb11bd64711d911e1e7
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2024-08-23 16:30:43 +0200

    fix deploy

--- scripts/06_deploy_testflight.sh
+++ scripts/06_deploy_testflight.sh
@@ -7,25 +7,27 @@ set -o pipefail
 echo "Changing to code directory at $CODE_DIR"
 pushd $CODE_DIR
 
+ARCHIVE_FILE=hello_world.ipa
+
 APPLE_ID_SECRET=apple-id
 APPLE_SECRET_SECRET=apple-secret
 APPLE_ID=$($AWS_CLI --region $REGION secretsmanager get-secret-value --secret-id $APPLE_ID_SECRET --query SecretString --output text)
 export APPLE_SECRET=$($AWS_CLI --region $REGION secretsmanager get-secret-value --secret-id $APPLE_SECRET_SECRET --query SecretString --output text)
 
-# echo "Verify Archive"
-# xcrun altool  \
-#             --validate-app \
-#             -f "build/ios/ipa/hello_world.ipa" \
-#             -t ios \
-#             -u $APPLE_ID \
-#             -p @env:APPLE_SECRET 
+echo "Verify Archive"
+xcrun altool  \
+            --validate-app \
+            -f "build/ios/ipa/${ARCHIVE_FILE}" \
+            -t ios \
+            -u $APPLE_ID \
+            -p @env:APPLE_SECRET 
 
-# echo "Upload to AppStore Connect"
-# xcrun altool  \
-# 		--upload-app \
-# 		-f "build/ios/ipa/hello_world.ipa" \
-# 		-t ios \
-# 		-u $APPLE_ID \
-# 		-p @env:APPLE_SECRET 
+echo "Upload to AppStore Connect"
+xcrun altool  \
+		--upload-app \
+		-f "build/ios/ipa/${ARCHIVE_FILE}" \
+		-t ios \
+		-u $APPLE_ID \
+		-p @env:APPLE_SECRET 
 
 popd
