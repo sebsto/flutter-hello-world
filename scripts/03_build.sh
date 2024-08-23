62af3d2d9181ff9f04d5267d205c16f7d34002f0
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2024-08-23 16:19:31 +0200

    fix build export to ipa

--- scripts/03_build.sh
+++ scripts/03_build.sh
@@ -10,6 +10,8 @@ pushd $CODE_DIR
 KEYCHAIN_PASSWORD=Passw0rd
 KEYCHAIN_NAME=dev.keychain
 
+EXPORT_OPTIONS_FILE="./exportOptions.plist"
+
 # Increase Build Number
 # https://rderik.com/blog/automating-build-and-testflight-upload-for-simple-ios-apps/
 
@@ -19,6 +21,35 @@ plutil -replace CFBundleVersion -string $BUILD_NUMBER "./getting started/Info.pl
 
 security unlock-keychain -p $KEYCHAIN_PASSWORD $KEYCHAIN_NAME
 
-$FLUTTER_DIR/bin/flutter build ipa
+cat << EOF > $EXPORT_OPTIONS_FILE
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>destination</key>
+	<string>export</string>
+	<key>method</key>
+	<string>app-store</string>
+	<key>provisioningProfiles</key>
+	<dict>
+		<key>com.stormacq.flutter.hello-world</key>
+		<string>flutter-hello-world</string>
+	</dict>
+	<key>signingCertificate</key>
+	<string>Apple Distribution</string>
+	<key>signingStyle</key>
+	<string>manual</string>
+	<key>stripSwiftSymbols</key>
+	<true/>
+	<key>teamID</key>
+	<string>56U756R2L2</string>
+	<key>uploadSymbols</key>
+	<true/>
+</dict>
+</plist>
+EOF
+
+$FLUTTER_DIR/bin/flutter config --no-cli-animations
+$FLUTTER_DIR/bin/flutter build ipa --export-options-plist $EXPORT_OPTIONS_FILE
 
 popd
