7b6d519d02d0d527c04aae2471d93e4923864f14
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2024-08-23 16:20:01 +0200

    start working on deploy

--- scripts/06_deploy_testflight.sh
+++ scripts/06_deploy_testflight.sh
@@ -7,11 +7,15 @@ set -o pipefail
 echo "Changing to code directory at $CODE_DIR"
 pushd $CODE_DIR
 
+APPLE_ID_SECRET=apple-id
+APPLE_SECRET_SECRET=apple-secret
+APPLE_ID=$($AWS_CLI --region $REGION secretsmanager get-secret-value --secret-id $APPLE_ID_SECRET --query SecretString --output text)
+export APPLE_SECRET=$($AWS_CLI --region $REGION secretsmanager get-secret-value --secret-id $APPLE_SECRET_SECRET --query SecretString --output text)
 
 # echo "Verify Archive"
 # xcrun altool  \
 #             --validate-app \
-#             -f "$BUILD_PATH/$SCHEME.ipa" \
+#             -f "build/ios/ipa/hello_world.ipa" \
 #             -t ios \
 #             -u $APPLE_ID \
 #             -p @env:APPLE_SECRET 
@@ -19,7 +23,7 @@ pushd $CODE_DIR
 # echo "Upload to AppStore Connect"
 # xcrun altool  \
 # 		--upload-app \
-# 		-f "$BUILD_PATH/$SCHEME.ipa" \
+# 		-f "build/ios/ipa/hello_world.ipa" \
 # 		-t ios \
 # 		-u $APPLE_ID \
 # 		-p @env:APPLE_SECRET 
