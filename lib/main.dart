c66448271d2b2404b69db75ccbc0aef00180714a
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2024-08-26 12:10:19 +0200

    test demo

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
         ),
         home: MyHomePage(),
       ),
